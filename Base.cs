2105a77ddf4ad0a9906bb97b73ea10ebbbd8f0f0
d.shkirando <d.shkirando@CMDB-137330>
2025-02-20 19:59:03 +0400

    First task calculator, first commit

---
+++ Base.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+
+namespace CalcV2
+{
+    public class Tests
+    {
+        protected WindowsDriver<WindowsElement> driver;
+        protected CalcPageObject calcPage;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new AppiumOptions();
+            options.PlatformName = "Windows";
+            options.AddAdditionalCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App"); 
+
+            driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723/"), options);
+            calcPage = new CalcPageObject(driver);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+                driver.Dispose();
+            }
+        }
+    }
+}
